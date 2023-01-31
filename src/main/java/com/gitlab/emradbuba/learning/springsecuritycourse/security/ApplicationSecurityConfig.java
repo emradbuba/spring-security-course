d9b694983110f7972e88af3a2c6e084cfe478f32
Radek Em <3488909-emradbuba@users.noreply.gitlab.com>
2023-01-31 08:55:30 +0100

    Add password encoder

--- src/main/java/com/gitlab/emradbuba/learning/springsecuritycourse/security/ApplicationSecurityConfig.java
+++ src/main/java/com/gitlab/emradbuba/learning/springsecuritycourse/security/ApplicationSecurityConfig.java
@@ -1,15 +1,31 @@
 package com.gitlab.emradbuba.learning.springsecuritycourse.security;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@Profile("original")
+@Slf4j
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        log.info("Configuring ORIGINAL security from course");
         http
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*")
@@ -18,5 +34,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        var userDetails = User
+                .withUsername("radek")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
